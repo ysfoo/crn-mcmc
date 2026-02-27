66bcaf6da174c9bd15d334f61437579c28acce1a
Yong See Foo <yongsee0819@yahoo.com>
2026-02-27 14:09:03 +1100

    Rerun MCMC with increasing nadapts if ESS < 500

--- insect/MCMC.jl
+++ insect/MCMC.jl
@@ -19,7 +19,10 @@ using AdvancedHMC, Bijectors, LinearAlgebra, LogDensityProblems, LogDensityProbl
 
 data = all_data[genmodel_idx];
 
-@showprogress for model_idx in 1:n_models    
+@showprogress for model_idx in 1:n_models 
+    mcmc_fname = joinpath(MCMCDIR, "MCMC_model$(model_idx).jld2")
+    isfile(mcmc_fname) && continue
+
     pmodel = create_petab_model(models[model_idx], data, u0)
     petab_prob = PEtabODEProblem(pmodel; odesolver=ODESolver(Rodas5P(), verbose=false))
     target = PEtabLogDensity(petab_prob);
@@ -32,22 +35,31 @@ data = all_data[genmodel_idx];
     end
 
     # run MCMC chain...
-    mcmc_fname = joinpath(MCMCDIR, "gm_MCMC_model$(model_idx).jld2")
-    if isfile(mcmc_fname)
-        continue
+    seed = model_idx
+    nadapts = 2000
+    while true
+        rng = StableRNG(seed)
+        chn = sample(
+            rng, turing_model(target), Turing.NUTS(0.9), 10000; 
+            initial_params=InitFromParams((θ=init_params,)), 
+            nadapts=nadapts, save_state=false, progress=false
+        );
+        acc_rate = round(mean(vec(chn[:acceptance_rate])); digits=4)
+        ess_df = ess(chn)
+        min_ess = round(Int, minimum(ess_df.nt.ess))
+        duration = round(MCMCChains.compute_duration(chn)/60; digits=2)
+        println("Model $(model_idx), acc rate = $acc_rate, min_ess = $min_ess, time = $duration min")    
+        flush(stdout)
+        flush(stderr)
+
+        if min_ess < 500
+            seed += 100
+            nadapts += 1000
+            continue
+        end
+
+        nMCMC = size(chn)[1]
+        @suppress_err @save mcmc_fname chn ess_df nMCMC;
+        break
     end
-    rng = StableRNG(model_idx)
-    chn = sample(
-        rng, turing_model(target), Turing.NUTS(0.9; metricT=AdvancedHMC.UnitEuclideanMetric), 10000; 
-        initial_params=InitFromParams((θ=init_params,)), 
-        nadapts=1000, save_state=false, progress=false
-    );
-    acc_rate = round(mean(vec(chn[:acceptance_rate])); digits=4)
-    ess_df = ess(chn)
-    min_ess = round(Int, minimum(ess_df.nt.ess))
-    println("Model $(model_idx), acc rate = $acc_rate, min_ess = $min_ess")    
-    nMCMC = size(chn)[1]
-    @suppress_err @save mcmc_fname chn ess_df nMCMC;
-    flush(stdout)
-    flush(stderr)
 end
